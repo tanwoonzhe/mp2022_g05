db38c291695ad6769a1bbb3509c5797ddb7a8be5
azheemsyahmee <azheem.syahmee@gmail.com>
2022-04-28 19:34:08 +0800

    Design
    List of book belum siap

---
+++ app/src/main/java/com/example/laibrary/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.example.laibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+    }
+}
